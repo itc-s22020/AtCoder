1b28f39262af236bc5a860b7717dd28d3aaf00dc
itc-s22020 <s22020@std.it-college.ac.jp>
2023-06-14 17:03:56 +0900

    深さ優先探索(DFS)の勉強

--- AtCoder-Beginner-Contest/algorithm/DepthFirstSearch.py
+++ AtCoder-Beginner-Contest/algorithm/DepthFirstSearch.py
@@ -1,1 +1,12 @@
 #深さ優先探索(Depth First Search)
+N,M = map(int, input().split())
+def Graph(n,m):
+    g = [[] for _ in range(n)]
+    for _ in range(m):
+        a, b = map(int, input().split())
+        g[a-1].append(b-1)
+        g[b-1].append(a-1)
+    return g
+G = Graph(N,M)
+print()
+print(G)
