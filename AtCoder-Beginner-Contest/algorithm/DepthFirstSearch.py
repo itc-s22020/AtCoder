e43b07efaa4550ed99347c74eabb24cc2bbaaca9
itc-s22020 <s22020@std.it-college.ac.jp>
2023-06-15 19:43:55 +0900

    コメントを追加

--- AtCoder-Beginner-Contest/algorithm/DepthFirstSearch.py
+++ AtCoder-Beginner-Contest/algorithm/DepthFirstSearch.py
@@ -13,14 +13,13 @@ def mkGraph(n,m):
 #深さ優先探索
 G = mkGraph(N,M)
 seen = [False for _ in range(len(G))]
-def DFS(g, n):
-    seen[n] = True
-    for v in g[n]:
-        if (seen[v]): continue
-        DFS(G, v)
-    
+def DFS(g, v):
+    seen[v-1] = True #探索済にする
 
+    # v から到達可能な next_v について
+    for next_v in g[v-1]:
+        if (seen[next_v-1]): continue # next_v が探索済ならスルー
+        DFS(G, next_v) #再帰処理
 
 DFS(G, 0)
-print(G)
 print(seen)
