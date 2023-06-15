a6bfad5aed5e098ced8f8c4e0ead1f50046cce9a
itc-s22020 <s22020@std.it-college.ac.jp>
2023-06-15 19:40:42 +0900

    深さ優先探索(DFS)をpythonで関数作成

--- AtCoder-Beginner-Contest/algorithm/DepthFirstSearch.py
+++ AtCoder-Beginner-Contest/algorithm/DepthFirstSearch.py
@@ -1,12 +1,26 @@
 #深さ優先探索(Depth First Search)
 N,M = map(int, input().split())
-def Graph(n,m):
+
+#グラフ作成
+def mkGraph(n,m):
     g = [[] for _ in range(n)]
     for _ in range(m):
         a, b = map(int, input().split())
-        g[a-1].append(b-1)
-        g[b-1].append(a-1)
+        g[a-1].append(b)
+        g[b-1].append(a) #有形グラフの場合はコメントアウト
     return g
-G = Graph(N,M)
-print()
+
+#深さ優先探索
+G = mkGraph(N,M)
+seen = [False for _ in range(len(G))]
+def DFS(g, n):
+    seen[n] = True
+    for v in g[n]:
+        if (seen[v]): continue
+        DFS(G, v)
+    
+
+
+DFS(G, 0)
 print(G)
+print(seen)
