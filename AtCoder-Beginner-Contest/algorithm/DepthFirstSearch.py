995d2d7e5b92686daea1bb7b5694e8580bd4a8d9
itc-s22020 <s22020@std.it-college.ac.jp>
2023-06-15 21:31:44 +0900

    頂点s~頂点vに到達可能か判定する関数StoVを実装

--- AtCoder-Beginner-Contest/algorithm/DepthFirstSearch.py
+++ AtCoder-Beginner-Contest/algorithm/DepthFirstSearch.py
@@ -2,31 +2,25 @@
 N,M = map(int, input().split())
 
 #グラフ作成
-def mkGraph(n,m):
+def mkGraph(n,m,T):
     g = [[] for _ in range(n)]
     for _ in range(m):
         a, b = map(int, input().split())
         g[a-1].append(b)
-        g[b-1].append(a) #有形グラフの場合はコメントアウト
+        if (T): g[b-1].append(a) #有形グラフの場合はコメントアウト
     return g
 
 #深さ優先探索
-G = mkGraph(N,M)
+G = mkGraph(N,M,False)
 seen = [False for _ in range(len(G))]
-first = [0 for _ in range(len(G))]
-def DFS(g, v, f):
-    #行きがけ順をメモ
-    first[v-1] += f
-    f += 1
-    
+def DFS(g, v):
     seen[v-1] = True #探索済にする
     # v から到達可能な next_v について
     for next_v in g[v-1]:
         if (seen[next_v-1]): continue # next_v が探索済ならスルー
-        DFS(G, next_v, f) #再帰処理
+        DFS(G, next_v) #再帰処理
 
 
-F = 0
-DFS(G, 0, F)
+DFS(G, 1)
+print(("NO", "YES")[seen[9]]) #頂点0から頂点9に辿り着けるか
 print(seen)
-print(first)
