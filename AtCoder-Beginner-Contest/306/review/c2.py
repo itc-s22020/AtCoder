d2b72c3555787e6225b5bc45249bf9b3b9c269db
itc-s22020 <s22020@std.it-college.ac.jp>
2023-06-20 09:35:24 +0900

    ABC306 C問題の解答コード実装

---
+++ AtCoder-Beginner-Contest/306/review/c2.py
@@ -0,0 +1,12 @@
+#解答コード実装
+n = int(input())
+a = list(map(int, input().split()))
+cnt = [0 for _ in range(3 * n)]
+ans = []
+for i in a:
+    cnt[i] += 1
+    if cnt[i] == 2:
+        ans.append(i)
+
+print(*ans)
+
