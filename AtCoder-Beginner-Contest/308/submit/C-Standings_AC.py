18c5d29e1fe6bf7ca8c0769cd9ccefe9b85d96d7
itc-s22020 <s22020@std.it-college.ac.jp>
2023-07-01 23:00:51 +0900

    ABC308のC問題提出結果(AC)

---
+++ AtCoder-Beginner-Contest/308/submit/C-Standings_AC.py
@@ -0,0 +1,23 @@
+#[AC] 514Byte 985ms 95508KB Python3
+from decimal import Decimal, getcontext
+
+def sort_people(N, A, B):
+    success_rates = [Decimal(A[i]) / (Decimal(A[i]) + Decimal(B[i])) for i in range(N)]
+    sorted_people = sorted(range(1, N+1), key=lambda i: (-success_rates[i-1], i))
+    return sorted_people
+
+N = int(input())
+A = []
+B = []
+for _ in range(N):
+    a, b = map(int, input().split())
+    A.append(a)
+    B.append(b)
+
+# Decimalの精度を設定
+getcontext().prec = 28
+
+sorted_people = sort_people(N, A, B)
+print(*sorted_people)
+
+
