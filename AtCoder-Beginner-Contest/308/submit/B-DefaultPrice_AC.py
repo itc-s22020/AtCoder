c3253758919825f1e9ff19dfd4110addadd3e026
itc-s22020 <s22020@std.it-college.ac.jp>
2023-07-01 22:58:14 +0900

    ABC308のB問題提出結果(AC)

---
+++ AtCoder-Beginner-Contest/308/submit/B-DefaultPrice_AC.py
@@ -0,0 +1,16 @@
+#[AC] 336Byte 25ms 9064KB Python3
+N, M = map(int, input().split())
+C = input().split()
+D = input().split()
+P = list(map(int, input().split()))
+defaultP = P.pop(0)
+total = 0
+for c in C:
+    if c in D:
+        for i,d in enumerate(D):
+            if c == d:
+                total+=P[i]
+                break
+    else:
+        total+=defaultP
+print(total)
